f4e1de5cd77262edae389b4f9d70760cad284529
Cainã Brazil <cbr.guaiamum@gmail.com>
2021-08-30 16:51:07 +0100

    Using container queries, added list container

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
+import { useContainerQuery } from "react-container-query";
+import classnames from "classnames";
+
 import Article from "./components/Article";
 import { commIcon, questionIcon, infoIcon } from "./assets/icons";
 
-// import ButtonRed from "./components/Button/ButtonRed";
-// import ButtonGreen from "./components/Button/ButtonGreen";
-import Button from "./components/Button";
+const query = {
+  'issue-desktop': {
+    minWidth: 768,
+  },
+};
 
 export default function App() {
+  const [params, containerRef] = useContainerQuery(query);
   return (
-    <main className="">
+    <>
+    <aside className="issue-list">
+      Things on the side
+    </aside>
+    <main className="max-h-screen">
       <h1 className="sticky top-0 shadow-md bg-white p-10 text-center text-3xl">
         I should be sticky on top!
       </h1>
-      <div className="grid issue">
+      <section ref={containerRef} className={classnames("grid issue", params)}>
         <Article title="Description" icon={infoIcon}>
           <strong>I should be first always.</strong> Aute nisi aute officia
           dolor aliqua quis anim. Esse ea mollit enim enim sint sit labore id
@@ -21,10 +31,6 @@ export default function App() {
           magna sunt id excepteur aute laborum aliquip tempor. Non quis tempor
           do laborum nulla eu dolore officia ad magna. Non voluptate sunt
           laboris qui duis commodo enim ad culpa.
-          {/* <ButtonRed text="I'm red!" /> */}
-          {/* <ButtonGreen text="I'm green!" /> */}
-          <Button color="Red" text="I'm red!" />
-          <Button color="Green" text="I'm green!" />
         </Article>
         <Article title="Details" icon={questionIcon} iconColor="gray">
           <p className="min-h-screen bg-gray-200">
@@ -34,10 +40,6 @@ export default function App() {
             Elit incididunt nostrud culpa proident est minim do ut duis fugiat
             id amet. Anim consectetur mollit aute do reprehenderit quis dolore
             tempor anim commodo dolor.
-            {/* <ButtonRed text="I'm red!" /> */}
-            {/* <ButtonGreen text="I'm green!" /> */}
-            <Button color="Red" text="I'm red!" />
-            <Button color="Green" text="I'm green!" />
           </p>
         </Article>
         <Article title="Comments" icon={commIcon} iconColor="yellow">
@@ -49,12 +51,9 @@ export default function App() {
                 <li className="p-2 h-16 border-2 rounded-md">{idx}</li>
               ))}
           </ul>
-          {/* <ButtonRed text="I'm red!" /> */}
-          {/* <ButtonGreen text="I'm green!" /> */}
-          <Button color="Red" text="I'm red!" />
-          <Button color="Green" text="I'm green!" />
         </Article>
-      </div>
+      </section>
     </main>
+    </>
   );
 }
